b1bd11e78c618cf7199935253e41cdf90b338b79
Tom White <tomwhite007@gmail.com>
2021-10-06 09:03:09 +0100

    chore: remove scaffolded gubbins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'simple-reactive-viewmodel-example';
-}
+export class AppComponent {}
