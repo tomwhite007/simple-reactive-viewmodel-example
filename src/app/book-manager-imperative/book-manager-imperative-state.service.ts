f79fa06a74c74fd3338b8b3cd6124851da063cec
Tom White <tomwhite007@gmail.com>
2021-10-11 07:57:20 +0100

    chore: remove unused deps

--- src/app/book-manager-imperative/book-manager-imperative-state.service.ts
+++ src/app/book-manager-imperative/book-manager-imperative-state.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { Subscription } from 'rxjs';
 import { BooksFacade } from '../+state/books.facade';
 import { BooksEntity } from '../+state/books.models';
 
